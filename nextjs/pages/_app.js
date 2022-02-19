fd997ce65d43b9dca4f55871f7fa49d94acf9dfa
Samyok Nepal <samyok@fiveable.me>
2022-02-19 10:49:06 -0600

    reorg

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </SessionProvider>
+    );
+}
